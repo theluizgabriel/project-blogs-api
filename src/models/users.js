3c65b00424c8f721a8c416429e1b57d5738df11f
Luiz Gabriel <lgabrielpessoal@gmail.com>
2022-09-23 15:16:34 -0300

    Iniciando o projeto + Users Migration & Model

---
+++ src/models/users.js
@@ -0,0 +1,23 @@
+// src/models/users.js
+
+module.exports = (sequelize, DataTypes) => {
+  const Users = sequelize.define('Users', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    display_name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    timestamps: false, // remove a obrigatoriedade de utilizar os campos `createdAt` e `updatedAt`
+    tableName: 'users',
+    underscored: true,
+  });
+
+  Users.associate = (models) => {
+    Users.hasMany(models.blogPosts,
+      { foreignKey: 'user_id', as: 'blog_posts' });
+  };
+
+  return Users;
+};
